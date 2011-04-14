aa8ad274fa47f727195aa968bb96c85ce4b40a7e
Chris Gilmer <chris.gilmer@ff0000.com>
2011-04-13 17:36:24 -0700

    Flushed the session cookie on logout.

--- social_auth/views.py
+++ social_auth/views.py
@@ -13,11 +13,7 @@ TWITTER_API_KEY     = getattr(settings, 'TWITTER_API_KEY', None)
 TWITTER_API_SECRET  = getattr(settings, 'TWITTER_API_SECRET', None)
 
 def logout(request):
-	if 'user' in request.session: del request.session['user']
-	if 'twitter_access_token' in request.session:
-		del request.session['twitter_access_token']
-	if 'facebook_access_token' in request.session:
-		del request.session['facebook_access_token']
+	request.session.flush()
 	return HttpResponseRedirect('/')
 
 def _get_access_token(request, provider):
