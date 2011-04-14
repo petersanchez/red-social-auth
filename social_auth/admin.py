c14be3cc46488097f4142ea0d2777cfa4625596e
Chris Gilmer <chris.gilmer@ff0000.com>
2011-04-14 11:42:18 -0700

    Added user images to the admin

--- social_auth/admin.py
+++ social_auth/admin.py
@@ -1,12 +1,18 @@
+from django.conf import settings
 from django.contrib import admin
 from social_auth.models import SocialUser, IdentityProvider
+
+def user_image(obj):
+	return ('<img src="%s" width="80" />' % (obj.image_url))
+user_image.short_description = 'User Image'
+user_image.allow_tags = True
 
 class IdentityProviderInline(admin.TabularInline):
 	model = IdentityProvider
 	extra = 0
 
 class SocialUserAdmin(admin.ModelAdmin): 
-	list_display  = ('username', 'image_url', 'created')
+	list_display  = ('username', user_image, 'created')
 	search_fields = ['username',]
 	list_filter   = ['created',]
 	inlines = [IdentityProviderInline,]
